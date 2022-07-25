3d89cd78e4992a3c2f4dc01a0a61cbf00d888947
yarab <yaraburvin@gmail.com>
2022-07-25 13:51:49 +0300

    Fizz-buzz function added

---
+++ src/utils/fizzBuzz.ts
@@ -0,0 +1,24 @@
+
+export function fizzBuzz(num : number ) : number | string {
+
+  let outCome : number | string;
+  if (((num % 3) == 0) && ((i % 5) == 0)) {
+    outCome = 'FizzBuzz'
+  }
+    
+  else if ((num % 3) == 0) {
+    outCome = 'Fizz'
+  }
+    
+  else if ((num % 5) == 0) {
+    outCome = 'Buzz'
+  }
+    
+  else {
+    outCome = num 
+  }
+  return outCome
+}
+
+
+
