8cb4561f20da5bf065caf70dd62db60059ca3bfd
yarab <yaraburvin@gmail.com>
2022-07-25 14:33:08 +0300

    App is working as needed

--- src/countNumbers.tsx
+++ src/countNumbers.tsx
@@ -2,27 +2,31 @@ import { useState } from "react"
 import {fizzBuzz} from "./utils/fizzBuzz" 
 
 export default function ButtonGameControl() : JSX.Element {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(1)
     const [storedCount, setStoredCount] = useState<number[]>([])
 
     const hadnleNumberCount = () => {
         setCount(prev => prev + 1);
         }
     const resetTheGame = () => {
-        setCount(0)
+        setCount(1)
         setStoredCount([])
     }
     const handleStoreNumbers = () => {
         setStoredCount([...storedCount, count])
     }
 
+    const combinedFunctions = ()=> {
+        handleStoreNumbers();
+        hadnleNumberCount()
+ }
     return (
         <>
         <h1>Fizz-Buzz game</h1>
-        <p>Game Field:</p>
-        {storedCount.map((value,index) => <p key={index}> {fizzBuzz(value)} </p>)}
+        <p>Game Field: {storedCount.map((value,index) => <span key={index}> {fizzBuzz(value)} </span>)}</p>
+        
         <hr />
-        <button onClick={() => {handleStoreNumbers(), hadnleNumberCount()}}>count!</button>
+        <button onClick={ combinedFunctions}>count!</button>
         <button onClick={resetTheGame}>Reset the game!</button>
         </>
         
