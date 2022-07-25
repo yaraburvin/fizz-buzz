73dbfedb6ae735ad4efa9f819e509d0007e1fe95
yarab <yaraburvin@gmail.com>
2022-07-25 14:43:58 +0300

    still resolving prettier issues

--- src/countNumbers.tsx
+++ src/countNumbers.tsx
@@ -1,33 +1,37 @@
-import { useState } from "react"
-import {fizzBuzz} from "./utils/fizzBuzz" 
+import { useState } from "react";
+import { fizzBuzz } from "./utils/fizzBuzz";
 
-export default function ButtonGameControl() : JSX.Element {
-    const [count, setCount] = useState(1)
-    const [storedCount, setStoredCount] = useState<number[]>([])
+export default function ButtonGameControl(): JSX.Element {
+  const [count, setCount] = useState(1);
+  const [storedCount, setStoredCount] = useState<number[]>([]);
 
-    const hadnleNumberCount = () => {
-        setCount(prev => prev + 1);
-        }
-    const resetTheGame = () => {
-        setCount(1)
-        setStoredCount([])
-    }
-    const handleStoreNumbers = () => {
-        setStoredCount([...storedCount, count])
-    }
+  const hadnleNumberCount = () => {
+    setCount((prev) => prev + 1);
+  };
+  const resetTheGame = () => {
+    setCount(1);
+    setStoredCount([]);
+  };
+  const handleStoreNumbers = () => {
+    setStoredCount([...storedCount, count]);
+  };
 
-    const combinedFunctions = ()=> {
-        handleStoreNumbers();
-        hadnleNumberCount()
- }
-    return (
-        <>
-        <h1>Fizz-Buzz game</h1>
-        <p>Game Field:   {storedCount.map((value,index) => <span key={index}> {fizzBuzz(value)} </span>)}</p>
-        <hr />
-        <button onClick={ combinedFunctions}>count!</button>
-        <button onClick={resetTheGame}>Reset the game!</button>
-        </>
-        
-    )
+  const combinedFunctions = () => {
+    handleStoreNumbers();
+    hadnleNumberCount();
+  };
+  return (
+    <>
+      <h1>Fizz-Buzz game</h1>
+      <p>
+        Game Field:{" "}
+        {storedCount.map((value, index) => (
+          <span key={index}> {fizzBuzz(value)} </span>
+        ))}
+      </p>
+      <hr />
+      <button onClick={combinedFunctions}>count!</button>
+      <button onClick={resetTheGame}>Reset the game!</button>
+    </>
+  );
 }
