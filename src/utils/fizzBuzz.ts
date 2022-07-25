244025b0660013ca92410696033ef263b1c8df9e
yarab <yaraburvin@gmail.com>
2022-07-25 14:51:20 +0300

    Ran lint

--- src/utils/fizzBuzz.ts
+++ src/utils/fizzBuzz.ts
@@ -1,10 +1,10 @@
 export function fizzBuzz(num: number): number | string {
   let outCome: number | string;
-  if (num % 3 == 0 && num % 5 == 0) {
+  if (num % 3 === 0 && num % 5 === 0) {
     outCome = "FizzBuzz";
-  } else if (num % 3 == 0) {
+  } else if (num % 3 === 0) {
     outCome = "Fizz";
-  } else if (num % 5 == 0) {
+  } else if (num % 5 === 0) {
     outCome = "Buzz";
   } else {
     outCome = num;
